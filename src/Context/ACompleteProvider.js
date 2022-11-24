bb834aa28a0a0b3128598b5b35e37d3592290e7e
msfk91 <msfk91@gmail.com>
2022-11-24 16:28:20 -0500

    Add files via upload

--- src/Context/ACompleteProvider.js
+++ src/Context/ACompleteProvider.js
@@ -1,1 +1,13 @@
-React Context for web app.
+import React, { useState, useContext} from "react"
+
+export const ACompleteContext = React.createContext([''])
+
+export function ACompleteProvider({children}){
+     const [AComplete, setAComplete] = useState([''])
+
+     return(
+        <ACompleteContext.Provider value={{AComplete, setAComplete}}>
+            {children}
+        </ACompleteContext.Provider>
+     )
+} 
